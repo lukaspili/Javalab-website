e574fa018df43d743a33692a61b86020209d51ce
Lukasz Piliszczuk <lukasz.pili@gmail.com>
2012-02-29 20:29:29 +0100

    Some clean on users controller.

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -37,9 +37,7 @@ public class Users extends AppController {
 
     @PublicAccess(only = true)
     public static void login() {
-
         pageHelper().addActionTitle();
-
         render();
     }
 
@@ -179,24 +177,6 @@ public class Users extends AppController {
         render(user);
     }
 
-
-    private static List<Campus> getCampusAsList() {
-        List<Campus> campusList = new ArrayList<Campus>();
-        for (Campus campus : Campus.values()) {
-            campusList.add(campus);
-        }
-        return campusList;
-    }
-
-    private static List<Promotion> getPromotionAsList() {
-        List<Promotion> promotionList = new ArrayList<Promotion>();
-        for (Promotion promotion : Promotion.values()) {
-            promotionList.add(promotion);
-        }
-        return promotionList;
-    }
-
-
     @LoggedAccess
     public static void modify(User user, String idBooster, Promotion promotion,
                               Campus campus) {
@@ -216,28 +196,9 @@ public class Users extends AppController {
     @LoggedAccess
     public static void profile() {
         User user = Auth.getCurrentUser();
-        List<Promotion> promotionList = getPromotionAsList();
+        List<Promotion> promotionList = Arrays.asList(Promotion.values());
         render(user, promotionList);
     }
-
-    @PublicAccess(only = true)
-    public static void inscription() {
-        List<Promotion> promotionList = getPromotionAsList();
-        List<Campus> campusList = getCampusAsList();
-        render(promotionList, campusList);
-    }
-
-//    @PublicAccess(only = true)
-//    public static void create(User user) {
-//        EnhancedValidator validator = validator();
-//        validator.validate(user).require("idBooster", "firstName", "lastName", "promotion", "campus");
-//        if (validator.hasErrors()) {
-//            render("Users/inscription.html", user);
-//        }
-//        User createdUser = userService.save(user);
-//        flash.success("Merci " + createdUser.firstName + createdUser.lastName + ", vous êtes bien enregistré");
-//        Dashboard.index();
-//    }
 
     @LoggedAccess
     public static void candidate() {
@@ -246,10 +207,4 @@ public class Users extends AppController {
         user.save();
         render(user);
     }
-
-
 }
-
-
-//http://www.suparis.fr/supinfoOpenIdPost.php?openid.claimed_id=https%3A%2F%2Fid.supinfo.com%2Fme%2F75054&openid.identity=https%3A%2F%2Fid.supinfo.com%2Fme%2F75054&openid.sig=IPxQNJWjhRJfaEaDcVhGtnhOUYGooC%2BGxlXlOBdWIvI%3D&openid
-// .signed=claimed_id%2Cidentity%2Cassoc_handle%2Cop_endpoint%2Creturn_to%2Cresponse_nonce%2Cns.alias3%2Calias3.mode%2Calias3.type.alias1%2Calias3.value.alias1%2Calias3.type.alias2%2Calias3.value.alias2%2Calias3.type.alias3%2Calias3.value.alias3%2Calias3.type.alias4%2Calias3.value.alias4%2Calias3.type.alias5%2Calias3.count.alias5%2Calias3.value.alias5.1%2Calias3.value.alias5.2%2Calias3.type.alias6%2Calias3.count.alias6%2Calias3.type.alias7%2Calias3.count.alias7%2Calias3.value.alias7.1%2Calias3.value.alias7.2%2Calias3.value.alias7.3%2Calias3.value.alias7.4%2Calias3.value.alias7.5&openid.assoc_handle=%7B634659297944867132%7D%7BSPwXyQ%3D%3D%7D%7B32%7D&openid.op_endpoint=https%3A%2F%2Fid.supinfo.com%2Fserver.aspx&openid.return_to=http%3A%2F%2Fwww.suparis.fr%2FsupinfoOpenIdPost.php&openid.response_nonce=2012-02-27T11%3A09%3A58ZcvXXcpCA&openid.mode=id_res&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.alias3=http%3A%2F%2Fopenid.net%2Fsrv%2Fax%2F1.0&openid.alias3.mode=fetch_response&openid.alias3.type.alias1=PersonRole&openid.alias3.value.alias1=Student&openid.alias3.type.alias2=PersonCampus&openid.alias3.value.alias2=305%3BSUPINFO%20Paris%3B&openid.alias3.type.alias3=PersonClasse&openid.alias3.value.alias3=A&openid.alias3.type.alias4=PersonCursus&openid.alias3.value.alias4=SIIT%3B4%3BWorldWide%3B&openid.alias3.type.alias5=PersonRank&openid.alias3.count.alias5=2&openid.alias3.value.alias5.1=Discover&openid.alias3.value.alias5.2=Advanced&openid.alias3.type.alias6=FullProfSubjects&openid.alias3.count.alias6=0&openid.alias3.type.alias7=TeacherSubjects&openid.alias3.count.alias7=5&openid.alias3.value.alias7.1=JAVA&openid.alias3.value.alias7.2=ORCL&openid.alias3.value.alias7.3=AAPL&openid.alias3.value.alias7.4=WWWT&openid.alias3.value.alias7.5=IMGT
