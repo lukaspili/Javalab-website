0d7d8f6f646ab08cf195c6a9a4e775ac08f89e2e
asmista <shuraval@yahoo.fr>
2012-11-21 20:52:11 +0800

    Maj details talk

--- app/seeds/TestSeed.java
+++ app/seeds/TestSeed.java
@@ -186,7 +186,8 @@ public class TestSeed {
         talk1.title = "Google map with Android";
         talk1.speaker = user6;
         talk1.date = new LocalDate(2011, 12, 23); // december 23, 2011
-        talk1.iframe = "<iframe width=\"560\" height=\"315\" src=\"http://www.youtube.com/embed/eTWfNdsrC9Q\" frameborder=\"0\" allowfullscreen></iframe>";
+        talk1.content = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
+        talk1.iframe = "<iframe width=\"920\" height=\"500\" src=\"http://www.youtube.com/embed/eTWfNdsrC9Q\" frameborder=\"0\" allowfullscreen></iframe>";
         talk1.save();
         
         Article article211111 = new Article();
