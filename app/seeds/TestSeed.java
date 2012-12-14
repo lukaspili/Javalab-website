c20ded8cee4d3dc3392a3aeba2b6f8b852b77790
asmista <shuraval@yahoo.fr>
2012-12-14 16:55:30 +0800

    Maj.

--- app/seeds/TestSeed.java
+++ app/seeds/TestSeed.java
@@ -200,7 +200,7 @@ public class TestSeed {
 
         Talk talk3 = new Talk();
         talk3.title = "Google map with Android";
-        talk3.speaker = user6;
+        talk3.speaker = user3;
         talk3.date = new LocalDate(2011, 12, 23); // december 23, 2011
         talk3.content = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
         talk3.iframe = "<iframe width=\"500\" height=\"280\" src=\"http://www.youtube.com/embed/eTWfNdsrC9Q\" frameborder=\"0\" allowfullscreen></iframe>";
@@ -208,7 +208,7 @@ public class TestSeed {
 
         Talk talk4 = new Talk();
         talk4.title = "Google map with Android";
-        talk4.speaker = user6;
+        talk4.speaker = user3;
         talk4.date = new LocalDate(2011, 12, 23); // december 23, 2011
         talk4.content = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
         talk4.iframe = "<iframe width=\"500\" height=\"280\" src=\"http://www.youtube.com/embed/eTWfNdsrC9Q\" frameborder=\"0\" allowfullscreen></iframe>";
@@ -216,7 +216,7 @@ public class TestSeed {
 
         Talk talk5 = new Talk();
         talk5.title = "Google map with Android";
-        talk5.speaker = user6;
+        talk5.speaker = user3;
         talk5.date = new LocalDate(2011, 12, 23); // december 23, 2011
         talk5.content = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
         talk5.iframe = "<iframe width=\"500\" height=\"280\" src=\"http://www.youtube.com/embed/eTWfNdsrC9Q\" frameborder=\"0\" allowfullscreen></iframe>";
@@ -224,7 +224,7 @@ public class TestSeed {
 
         Talk talk6 = new Talk();
         talk6.title = "Google map with Android";
-        talk6.speaker = user6;
+        talk6.speaker = user3;
         talk6.date = new LocalDate(2011, 12, 23); // december 23, 2011
         talk6.content = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
         talk6.iframe = "<iframe width=\"500\" height=\"280\" src=\"http://www.youtube.com/embed/eTWfNdsrC9Q\" frameborder=\"0\" allowfullscreen></iframe>";
